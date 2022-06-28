711cab3f9cbee53fb85c76b8aaa449c218754688
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-28 11:42:34 +0300

    IRepository interfaces has been created

---
+++ AuthServer.Core/Reposityories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Reposityories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+
+        Task<TEntity> GetBtIdAsync(int id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);   
+        Task AddAsunc(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Remove(TEntity entity);
+
+    }
+}

00a8377685bda927b7047b5b41d8f3d97bf0c64b
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-30 09:09:53 +0300

    fixed

--- AuthServer.Core/Models/UserRefreshToken.cs
+++ AuthServer.Core/Models/UserRefreshToken.cs
@@ -9,7 +9,9 @@ namespace AuthServer.Core.Models
     public class UserRefreshToken
     {
         public string UserId { get; set; }
+
         public string Code { get; set; }
+
         public DateTime Expiration { get; set; }
     }
 }

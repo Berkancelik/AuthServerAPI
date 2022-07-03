ed10794dec21a0f9d3744ea3d48a889ae014b228
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-03 11:11:54 +0300

    Client and ITokenservice has been created

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+    public class Client
+    {
+        public string ClientId { get; set; }
+        public string Secret { get; set; }
+        public List<String> Auidences { get; set; }
+
+    }
+}
---
+++ AuthServer.Core/Repositories/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.Configuration;
+using AuthServer.Core.DTOs;
+using AuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateClientToken(Client userApp);
+    }
+}

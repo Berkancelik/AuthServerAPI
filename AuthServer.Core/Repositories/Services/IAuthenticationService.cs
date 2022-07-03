3947ba253c7b52ef1f49458901fff6fcd38f2e45
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-03 11:34:43 +0300

    IAuthenticationServie interface has been created

---
+++ AuthServer.Core/Repositories/Services/IAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateByRefreshTokenAsync(string refreshToken);      
+        Task<Response<NoDataDto>> RevokeRefrehToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientTokenDto clientTokenDto);
+
+
+    }
+}

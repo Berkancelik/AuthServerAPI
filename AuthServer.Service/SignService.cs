08c739705085f1837fd113108a642d28bb09dc4c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-04 11:26:41 +0300

    SignService has been created

---
+++ AuthServer.Service/SignService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service
+{
+    public static class SignService
+    {
+        //şifreleme yöntemi olarak da blinmektedir bu asimetrik imzalama 
+        public static SecurityKey GeSymetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
